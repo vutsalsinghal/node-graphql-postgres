627a5349b537334b9d89bbe2f1b686ebfb41d592
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-03-02 03:01:35 -0500

    added friend table

--- schema.js
+++ schema.js
@@ -43,37 +43,51 @@ const RootQuery = new GraphQLObjectType({
     },
     user: {
       type: UserType,
-      args: {
-        id: { type: GraphQLInt }
-      },
-      async resolve(parentValue, args) {
-        var res = await knex.select().table("user")
-        for (var i = 0; i < data.length; i++) {
-          if (data[i].id == args.id) {
-            return data[i]
-          }
+      async resolve(parentValue, args, { session }) {
+        if (!session.user) {
+          throw new Error("Not logged in!");
+        } else {
+          var res = await knex.select().table("user").where("id", session.user.id)
+          return res[0]
         }
-        return res
       }
     },
     getCalorieIntake: {
       type: CalorieType,
-      args: {
-        uid: { type: new GraphQLNonNull(GraphQLInt) }
-      },
-      async resolve(parentValue, args) {
-        var res = await knex.select().table("calorie-intake").where("user_id", args.uid)
-        return { id: res[0].id, user_id: res[0].user_id, amount: res[0].intake }
+      async resolve(parentValue, args, { session }) {
+        if (!session.user) {
+          throw new Error("Not logged in!");
+        } else {
+          var res = await knex.select().table("calorie-intake").where("user_id", session.user.id)
+          return { id: res[0].id, user_id: res[0].user_id, amount: res[0].intake }
+        }
       }
     },
     getCalorieSpent: {
       type: CalorieType,
-      args: {
-        uid: { type: new GraphQLNonNull(GraphQLInt) }
-      },
-      async resolve(parentValue, args) {
-        var res = await knex.select().table("calorie-spent").where("user_id", args.uid)
-        return { id: res[0].id, user_id: res[0].user_id, amount: res[0].spent }
+      async resolve(parentValue, args, { session }) {
+        if (!session.user) {
+          throw new Error("Not logged in!");
+        } else {
+          let res = await knex.select().table("calorie-spent").where("user_id", session.user.id)
+          return { id: res[0].id, user_id: res[0].user_id, amount: res[0].spent }
+        }
+      }
+    },
+    getFriends: {
+      type: new GraphQLList(UserType),
+      async resolve(parentValue, args, { session }) {
+        if (!session.user) {
+          throw new Error("Not logged in!");
+        } else {
+          let friends = await knex.select("friend_id").table("friend").where("user_id", session.user.id)
+          let result = [];
+          for (let i = 0; i < friends.length; i++) {
+            let res = await knex.select().table("user").where("id", friends[i].friend_id)
+            result.push({ id: res[0].id, username: res[0].username });
+          }
+          return result
+        }
       }
     }
   }
@@ -156,6 +170,30 @@ const mutationQuery = new GraphQLObjectType({
           let currentSpent = await knex.select("id", "spent").table("calorie-spent").where("user_id", session.user.id)
           let res = await knex("calorie-spent").where("user_id", '=', session.user.id).update({ spent: currentSpent[0].spent + args.amount }, ["id", "user_id", "spent"])
           return { id: res[0].id, user_id: res[0].user_id, amount: res[0].spent }
+        } else {
+          throw new Error("Not logged in!");
+        }
+      }
+    },
+    addfriend: {
+      type: GraphQLString,
+      args: {
+        fid: { type: new GraphQLNonNull(GraphQLInt) }
+      },
+      async resolve(parentValue, args, { session }) {
+        if (session.user) {
+          if (session.user.id == args.fid) {
+            throw new Error("uid cannot be equal to fid!");
+          } else {
+            let isFriend = await knex.select("user_id", "friend_id").table("friend").where({ user_id: session.user.id, friend_id: args.fid })
+            if (isFriend.length === 1) {
+              throw new Error("Is an existing friend!");
+            } else {
+              await knex("friend").insert({ user_id: session.user.id, friend_id: args.fid })
+              await knex("friend").insert({ user_id: args.fid, friend_id: session.user.id })
+            }
+          }
+          return "Friend added!"
         } else {
           throw new Error("Not logged in!");
         }
